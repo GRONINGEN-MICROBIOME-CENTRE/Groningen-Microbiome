09bcfce7ff1fc89ca7e205b4600117a6083b573c
LianminChen <32386784+LianminChen@users.noreply.github.com>
2019-09-16 10:30:08 +0200

    Update PanPhlAn_analysis_pipeline.sh

--- Scripts/PanPhlAn_analysis_pipeline.sh
+++ Scripts/PanPhlAn_analysis_pipeline.sh
@@ -1,3 +1,3 @@
 ##if you have any questions please contact: lianminchen@yeah.net
-##the panphlan including two parts, first part for creating genome scanning (present or absent of genes) file from MGS for certain species, second part will merge all genome scanning files  (each samples one marker file) you show interest and do downstream analysis
+##the panphlan including two parts, first part for creating genome scanning file (present or absent of genes) from MGS for certain species, second part will merge all genome scanning files  (each samples one marker file) you show interest and do downstream analysis
 ##run panphlan for .bam files as an example in Boxy cluster
