ad90270d2b7a4a099cbf09cf4fe0381222bbe20d
ealopera <44577866+ealopera@users.noreply.github.com>
2020-03-06 11:28:13 +0100

    Changed filters and added flagging for each feature in both steps

--- Scripts/DAG3_GWAS/5.GWAS_interpret_plot/launch.job.5.gwasplot.sh
+++ Scripts/DAG3_GWAS/5.GWAS_interpret_plot/launch.job.5.gwasplot.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=60:59:59
+#SBATCH --time=59:59:59
 #SBATCH --mem=48gb
+#SBATCH --output=/groups/umcg-wijmenga/tmp04/umcg-elopera/DAG3/scripts/graph_0.5n_filter.out
+#SBATCH --error=/groups/umcg-wijmenga/tmp04/umcg-elopera/DAG3/scripts/graph_0.5n_filter.err
 
 ## added full path to rscript
 ml RPlus
 hostname
 
 
-Rscript /groups/umcg-ugli/tmp01/umcg-elopera/testDAG/scripts/5.mb_GWAS_merge_plot.R -i /groups/umcg-ugli/tmp01/umcg-elopera/testDAG/output/out_v2_taxa \
-                                                                     -m /groups/umcg-ugli/tmp01/umcg-elopera/testDAG/output/out_v2_taxa/gwas.list 
+Rscript /groups/umcg-wijmenga/tmp04/umcg-elopera/DAG3/scripts/5.mb_GWAS_merge_plot.R -i /groups/umcg-wijmenga/tmp04/umcg-elopera/DAG3/output/out_v2 \
+                                                                                     -m /groups/umcg-wijmenga/tmp04/umcg-elopera/DAG3/output/out_v2/to.plot.2 
