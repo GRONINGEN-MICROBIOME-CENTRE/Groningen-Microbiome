d5dc97444d57316cf3ed604b4fef76dc04e3c1cb
shixianhu <32686413+shixianhu@users.noreply.github.com>
2020-05-21 16:54:49 +0200

    Update Split.batch.sh

--- Projects/Fecal_metabolome_IBD/Metabolites_GWAS/Split.batch.sh
+++ Projects/Fecal_metabolome_IBD/Metabolites_GWAS/Split.batch.sh
@@ -1,10 +1,10 @@
 # split traits file into multiple batches, keep header
 # adjust parameter "l" to define line number in each batch (the number of traits)
 
-awk -v l=50 '(NR==1){header=$0;next}
+awk -v l=10 '(NR==1){header=$0;next}
                 (NR%l==2) {
                    close(file); 
-                   file=sprintf("%s.%0.5d.csv",FILENAME,++c)
+                   file=sprintf("%s.%0.5d.txt",FILENAME,++c)
                    sub(/csv[.]/,"",file)
                    print header > file
                 }
