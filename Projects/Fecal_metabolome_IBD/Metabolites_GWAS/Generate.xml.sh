4ce9ac307cd2936726c5967c35e95e62700f84f4
shixianhu <32686413+shixianhu@users.noreply.github.com>
2020-05-21 16:34:59 +0200

    Update Generate.xml.sh

--- Projects/Fecal_metabolome_IBD/Metabolites_GWAS/Generate.xml.sh
+++ Projects/Fecal_metabolome_IBD/Metabolites_GWAS/Generate.xml.sh
@@ -13,6 +13,7 @@ do
 mkdir -p /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch
 
 cp /groups/umcg-gastrocol/tmp04/Metabolic_Project/MetaAnalysis_numeric_template/Marc_XML.chr.tmp.xml /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/
+cp /groups/umcg-gastrocol/tmp04/Metabolic_Project/MetaAnalysis_numeric_template/MetaAnalysis.chr.tmp.sh /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/
 
 for chr in {1..22}
 
@@ -22,36 +23,40 @@ mkdir -p /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batc
 final_out="/groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Output_$chr/"
 
 sed "s|final_out|${final_out}|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.tmp.xml \
-> /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+> /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
-sed -i "s|lld_genetic_input|${lld_genetic_input}\/trityper_${chr}|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+sed -i "s|lld_genetic_input|${lld_genetic_input}\/trityper_${chr}|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
-sed -i "s|cd_genetic_input|${cd_genetic_input}\/trityper_${chr}|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+sed -i "s|cd_genetic_input|${cd_genetic_input}\/trityper_${chr}|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
-sed -i "s|uc_genetic_input|${uc_genetic_input}\/trityper_${chr}|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+sed -i "s|uc_genetic_input|${uc_genetic_input}\/trityper_${chr}|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
-sed -i "s|lld_coupling|${coupling}\/LLD.coupling.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+sed -i "s|lld_coupling|${coupling}\/LLD.coupling.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
-sed -i "s|cd_coupling|${coupling}\/CD.coupling.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+sed -i "s|cd_coupling|${coupling}\/CD.coupling.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
 sed -i "s|uc_coupling|${coupling}\/UC.coupling.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.tmp.xml
 
-sed -i "s|lld_trait|${trait}\/CT_numeric.metabolic.000${batch}.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+sed -i "s|lld_trait|${trait}\/CT_numeric.metabolic.000${batch}.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
-sed -i "s|cd_trait|${trait}\/CD_numeric.metabolic.000${batch}.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml  
+sed -i "s|cd_trait|${trait}\/CD_numeric.metabolic.000${batch}.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
-sed -i "s|uc_trait|${trait}\/UC_numeric.metabolic.000${batch}.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+sed -i "s|uc_trait|${trait}\/UC_numeric.metabolic.000${batch}.txt|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
-sed -i "s|lld_annot|${trait}\/CT_numeric.metabolic.txt.annot|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+sed -i "s|lld_annot|${trait}\/CT_numeric.metabolic.txt.annot|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
-sed -i "s|cd_annot|${trait}\/CD_numeric.metabolic.txt.annot|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+sed -i "s|cd_annot|${trait}\/CD_numeric.metabolic.txt.annot|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
 
-sed -i "s|uc_annot|${trait}\/UC_numeric.metabolic.txt.annot|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml 
+sed -i "s|uc_annot|${trait}\/UC_numeric.metabolic.txt.annot|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/Marc_XML.chr.${chr}.xml
+
+sed "s|batchtmp|${batch}|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/MetaAnalysis.chr.tmp.sh \
+> /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/MetaAnalysis.chr.${chr}.sh
+
+sed -i "s|chrtmp|${chr}|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/MetaAnalysis.chr.${chr}.sh
+sed -i "s|Marc_XML.xml|Marc_XML.chr.${chr}.xml|g" /groups/umcg-gastrocol/tmp04/Metabolic_Project/Numeric_meta/Batch.$batch/MetaAnalysis.chr.${chr}.sh
 
 echo -e "batch $batch ---- chr $chr is done"
 
 done
 
 done
-
-
