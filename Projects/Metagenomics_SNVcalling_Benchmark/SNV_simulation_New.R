d54a9488047e4c46a64eba2463198f4940c07c2d
Sergio Andreu-Sanchez <s.andreu.sanchez@umcg.nl>
2020-11-20 09:20:31 +0100

    Cluster script

--- Projects/Metagenomics_SNVcalling_Benchmark/SNV_simulation_New.R
+++ Projects/Metagenomics_SNVcalling_Benchmark/SNV_simulation_New.R
@@ -1,9 +1,9 @@
-setwd("/Users/sergio/Documents/PhD/SNV_calling/")
 library(tidyverse)
 library(viridis)
+.libPaths("R_packages")
 library(patchwork)
-MS = read_tsv("Benchmark_MS.tsv")
-SS = read_tsv("Benchmark_SS.tsv")
+MS = read_tsv("Simulation_Paper_2Refs/Benchmark.tsv")
+SS = read_tsv("Simulation_Paper/Benchmark.tsv")
 SS$Benchmark_Design = "uniref"
 
 
@@ -34,24 +34,31 @@ for (Data in list(MS, SS)){
   
   Sensitivity_plot / Precision_plot / (TP_plot + FN_plot + FP_plot) | Summary_plot +  plot_layout(guides = "collect") +   plot_annotation(title =unique(Data$Benchmark_Design) )  -> Composition
   print(Composition)
+
+  ggsave(paste(c("Plots/",unique(Data$Benchmark_Design),"General.pdf"),collapse=""),plot = Composition, height=13, width=13, units="in")
+  ggsave(paste(c("Plots/",unique(Data$Benchmark_Design),"General.png"), collapse=""),plot = Composition, height=13, width=13, units="in")
   
 }
 
-ggsave(filename = "Plots/Call_plot.png",Composition, width = 13 ,height = 13 , units = "in")
+MS_R = read_tsv("Simulation_Paper_2Refs/ROC_curves.tsv") %>% mutate(Benchmark_Design="multiref")
+SS_R =  read_tsv("Simulation_Paper/ROC_curves.tsv") %>% mutate(Benchmark_Design="uniref")
 
-SS_R = read_tsv("ROC_curves_SS.tsv")
-SS_R %>% drop_na() %>% group_by(Tool,Quantile_Phred)  %>% summarise(M_Precision = mean(Precision), SD_Precision = sd(Precision),M_sensitivity = mean(Sensitivity), SD_sensitivity = sd(Sensitivity) ) -> ROC_sum
-ROC_sum %>% 
-  mutate(Up_Precision = M_Precision+SD_Precision, Down_Precision = M_Precision-SD_Precision, UP_Sensitivity = M_sensitivity+SD_sensitivity, Down_sensitivity = M_sensitivity-SD_sensitivity ) -> ROC_sum
-ROC_sum %>% mutate(Down_Precision = ifelse(Down_Precision<0,0,Down_Precision), Down_sensitivity=ifelse(Down_sensitivity<0,0,Down_sensitivity)) -> ROC_sum
-#Recall (TP / TP+FN ) , Sensitivity, True Positive Rate
-ROC_sum %>% ggplot(aes(y=M_sensitivity, x=Quantile_Phred)) + geom_line(aes( col=Tool)) + theme_bw() + scale_color_manual(values = wsjPal)  + ylab("Mean sensitivity (TP/TP+FN)") +
-  geom_ribbon(aes(ymin=Down_sensitivity, ymax=UP_Sensitivity, fill=Tool), linetype=2, alpha=0.05)+ scale_fill_manual(values = wsjPal)  -> RSensitivity_plot
 
-#Precision (TP/ TP+FP) , Positive Prediction value
-ROC_sum %>% ggplot(aes(y=M_Precision, x=Quantile_Phred)) + geom_line(aes(col=Tool)) + theme_bw()+ scale_color_manual(values=wsjPal)    + ylab("Mean Precision (TP/TP+FP)") +
-  geom_ribbon(aes(ymin=Down_Precision, ymax=Up_Precision, fill=Tool), linetype=2, alpha=0.05) + scale_fill_manual(values = wsjPal)  -> RPrecision_plot
+for (ROC in list(MS_R, SS_R)){
+	
+	ROC %>% drop_na() %>% group_by(Tool,Quantile_Phred)  %>% summarise(M_Precision = mean(Precision), SD_Precision = sd(Precision),M_sensitivity = mean(Sensitivity), SD_sensitivity = sd(Sensitivity) ) -> ROC_sum
+	ROC_sum %>% mutate(Up_Precision = M_Precision+SD_Precision, Down_Precision = M_Precision-SD_Precision, UP_Sensitivity = M_sensitivity+SD_sensitivity, Down_sensitivity = M_sensitivity-SD_sensitivity ) -> ROC_sum
+	ROC_sum %>% mutate(Down_Precision = ifelse(Down_Precision<0,0,Down_Precision), Down_sensitivity=ifelse(Down_sensitivity<0,0,Down_sensitivity)) -> ROC_sum
 
-RSensitivity_plot | RPrecision_plot  +  plot_layout(guides = "collect") -> Composition2
-ggsave(filename = "Plots/ROC_probabilistic.png",Composition2, width = 13 ,height = 13 , units = "in")
+	#Recall (TP / TP+FN ) , Sensitivity, True Positive Rate
+	ROC_sum %>% ggplot(aes(y=M_sensitivity, x=Quantile_Phred)) + geom_line(aes( col=Tool)) + theme_bw() + scale_color_manual(values = wsjPal)  + ylab("Mean sensitivity (TP/TP+FN)") +
+	  geom_ribbon(aes(ymin=Down_sensitivity, ymax=UP_Sensitivity, fill=Tool), linetype=2, alpha=0.05)+ scale_fill_manual(values = wsjPal)  -> RSensitivity_plot
 
+	#Precision (TP/ TP+FP) , Positive Prediction value
+	ROC_sum %>% ggplot(aes(y=M_Precision, x=Quantile_Phred)) + geom_line(aes(col=Tool)) + theme_bw()+ scale_color_manual(values=wsjPal)    + ylab("Mean Precision (TP/TP+FP)") +
+	  geom_ribbon(aes(ymin=Down_Precision, ymax=Up_Precision, fill=Tool), linetype=2, alpha=0.05) + scale_fill_manual(values = wsjPal)  -> RPrecision_plot
+
+	RSensitivity_plot | RPrecision_plot  +  plot_layout(guides = "collect") -> Composition2
+	ggsave(paste(c("Plots/",unique(ROC$Benchmark_Design),"ROC_probabilistic.pdf"),collapse=""),plot = Composition2, height=13, width=13, units="in")
+	ggsave(paste(c("Plots/",unique(ROC$Benchmark_Design),"ROC_probabilistic.png"), collapse=""),plot = Composition2, height=13, width=13, units="in")
+}
