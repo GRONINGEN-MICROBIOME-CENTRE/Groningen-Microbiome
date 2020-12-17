77006f229d6ec563557ee80e05fb2b385a9c6b0e
Ranko Gacesa, UMCG <r.gacesa@umcg.nl>
2020-12-17 10:28:29 +0100

    Update LL11_A01_871_vs.sh

--- Projects/DMP/microbiome_profiling/LL11_A01_871_vs.sh
+++ Projects/DMP/microbiome_profiling/LL11_A01_871_vs.sh
@@ -14,6 +14,3 @@ echo "Running VFDB_SB analysis"
 /data/umcg-rgacesa/tools/shortBRED/shortbred_quantify.py --markers /data/umcg-rgacesa/tools/shortBRED/markers_VFDB/markers_core_VFDB.fa --wgs LL11_A01_871/clean_reads/LL11_A01_871_kneaddata_merged.fastq --threads 16 --results LL11_A01_871/VFDB_SB/LL11_A01_871_vfdb_sb.txt --tmp LL11_A01_871/VFDB_SB/tmp --usearch /data/umcg-rgacesa/tools/usearch/usearch10.0.240_i86linux32
 NLINES=$(grep -c '' LL11_A01_871/clean_reads/LL11_A01_871_1_kneaddata_paired_1.fastq)
 NREADS=$(( NLINES / 4 ))
-# --- PARSE shortBRED results
-python /data/umcg-rgacesa/tools/DB_VFDB/parseVFDBshortBRED.py --inFile LL11_A01_871/VFDB_SB/LL11_A01_871_vfdb_sb.txt --annot '/data/umcg-rgacesa/tools/shortBRED/markers_VFDB/VFDB_annotation.csv' --out LL11_A01_871/VFDB_SB/LL11_A01_871_vfdb_sb_out --fasta /data/umcg-rgacesa/tools/shortBRED/markers_VFDB/VFDB_core_annotated_sb.fa --readN $NREADS
-rm -r LL11_A01_871/VFDB_SB/tmp
